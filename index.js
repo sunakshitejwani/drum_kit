09948611a54de3ebae9b4669ad729a0d353a9aa4
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-11 19:52:22 +0530

    add audio

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("hello");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
   });
-}
-
-document.querySelector(".drum").addEventListener("click", function() {
-  alert("hello");
-});
-
-function handleClick() {
-  alert("hello");
 }
