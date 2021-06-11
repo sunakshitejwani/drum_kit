bfecbf562a4f25ef9e184aa973083a83af44c06d
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-11 20:16:05 +0530

    replace if with switch

--- index.js
+++ index.js
@@ -2,33 +2,39 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    // var audio = new Audio("sounds/tom-1.mp3");
-    // audio.play();
-    //console.log(this.innerHTML);
     var audio;
-    this.style.color = "white";
     let identity = this.innerHTML;
-    if (identity === "w") {
-      audio = new Audio("sounds/crash.mp3");
-      audio.play();
-    } else if (identity === "a") {
-      audio = new Audio("sounds/kick-bass.mp3");
-      audio.play();
-    } else if (identity === "s") {
-      audio = new Audio("sounds/snare.mp3");
-      audio.play();
-    } else if (identity === "d") {
-      audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
-    } else if (identity === "j") {
-      audio = new Audio("sounds/tom-2.mp3");
-      audio.play();
-    } else if (identity === "k") {
-      audio = new Audio("sounds/tom-3.mp3");
-      audio.play();
-    } else if (identity === "l") {
-      audio = new Audio("sounds/tom-4.mp3");
-      audio.play();
+    switch (identity) {
+      case "w":
+        audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+      case "a":
+        audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+      case "s":
+        audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+      case "d":
+        audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "j":
+        audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "k":
+        audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "l":
+        audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+      default:
+        console.log(identity);
     }
   });
 }
