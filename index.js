592ff12b2731c434efd8bd1f1e33a7fc4ab25e39
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-11 20:00:54 +0530

    Add audio to all the images

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+    //console.log(this.innerHTML);
+    var audio;
+    this.style.color = "white";
+    let identity = this.innerHTML;
+    if (identity === "w") {
+      audio = new Audio("sounds/crash.mp3");
+      audio.play();
+    } else if (identity === "a") {
+      audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+    } else if (identity === "s") {
+      audio = new Audio("sounds/snare.mp3");
+      audio.play();
+    } else if (identity === "d") {
+      audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+    } else if (identity === "j") {
+      audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+    } else if (identity === "k") {
+      audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+    } else if (identity === "l") {
+      audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+    }
   });
 }
